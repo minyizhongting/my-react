cf980536db7336e52f589cf4c914892e75540c93
JDJR\baihe3 <baihe3@jd.com>
2018-08-30 19:23:50 +0800

    hotfix

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -44,5 +45,10 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
+    // new HtmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: './src/index.html',
+    //   favicon: './src/favicon.ico'
+    // })
   ]
 };
